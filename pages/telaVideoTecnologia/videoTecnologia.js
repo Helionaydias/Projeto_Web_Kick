a74c266c6a699763c37be51fd043d1698e886df9
helionay <helionaydias@hotmail.com>
2024-07-10 20:26:27 -0300

    Adicionando arquivos de vídeo grandes para serem rastreados pelo Git LFS

---
+++ pages/telaVideoTecnologia/videoTecnologia.js
@@ -0,0 +1,10 @@
+// let myVideo = document.querySelector('.my-video');
+
+// function playPause() {
+//   if (myVideo.paused) {
+//     myVideo.play();
+//   } else {
+//     myVideo.pause();
+//   }
+
+// }
