f208804444beb812a9cda096b55cfa24bb70c017
helionay <helionaydias@hotmail.com>
2024-07-18 15:41:29 -0300

    ajuste css calendário e login e javascript calendário e login

--- pages/telaLogin/login.js
+++ pages/telaLogin/login.js
@@ -1,26 +1,24 @@
-// login.js
-
-// Função para manipular o clique do botão de entrar
 document.getElementById('submit').addEventListener('click', function () {
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
 
-  // Exemplo de validação básica
   if (email === '' || senha === '') {
     alert('Por favor, preencha todos os campos.');
     return;
   }
 
-  // Simulação de envio de dados (substituir com lógica real de autenticação)
   console.log('Email:', email);
   console.log('Senha:', senha);
-  alert('Login realizado com sucesso!');
 
-  // Limpa os campos após o login
+  if (true) {
+    window.location.href = '../../index.html';
+  } else {
+    alert('Falha no login. Por favor, tente novamente.');
+  }
+
   document.getElementById('dados').reset();
 });
 
-// Função para manipular o clique do botão de "Esqueceu a Senha"
 document
   .getElementById('botaoEsqueceuSenha')
   .addEventListener('click', function () {
