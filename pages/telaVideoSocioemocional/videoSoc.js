372e00dcf6b33e6f7cef0b9c8ab580771fbe5bb5
helionay <helionaydias@hotmail.com>
2024-07-21 08:11:45 -0300

    inserindo javascript na page telaVideoSocioemocional

--- pages/telaVideoSocioemocional/videoSoc.js
+++ pages/telaVideoSocioemocional/videoSoc.js
@@ -1,22 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const notificationSound = new Audio(
-    '../../pages/telaVideoSocioemocional/media/notification_sound.mp3',
-  );
-  const notificationBadge = document.getElementById('notification-badge');
-  const newNotification = document.getElementById('new-notification');
-
-  newNotification.addEventListener('click', function () {
-    notificationSound.play();
-    let currentCount = parseInt(notificationBadge.textContent);
-    notificationBadge.textContent = currentCount + 1;
-  });
-
   const video = document.getElementById('my-video');
   const playButton = document.getElementById('play-button');
   const pauseButton = document.getElementById('pause-button');
 
   playButton.addEventListener('click', function () {
-    video.play();
+    video.play().catch((error) => {
+      console.error('Playback prevented: ', error);
+    });
   });
 
   pauseButton.addEventListener('click', function () {
