451e10e47db6878fe41ba7a906993a24b2311c05
helionay <helionaydias@hotmail.com>
2024-07-23 14:18:28 -0300

    ajuste js videoSoc

--- pages/telaVideoSocioemocional/videoSoc.js
+++ pages/telaVideoSocioemocional/videoSoc.js
@@ -1,24 +1,31 @@
 var myVideo = document.getElementById('video1');
 var isPlaying = false;
+var isTransitioning = false;
 
 function playPause() {
-  if (isPlaying) {
-    myVideo.pause();
-  } else {
-    myVideo.play().catch((error) => {
-      console.error('Erro ao tentar reproduzir o vídeo:', error);
-    });
+  if (!isTransitioning) {
+    if (isPlaying) {
+      myVideo.pause();
+    } else {
+      myVideo.play().catch((error) => {
+        console.error('Erro ao tentar reproduzir o vídeo:', error);
+      });
+    }
+    isTransitioning = true;
   }
 }
 
 myVideo.onplaying = function () {
   isPlaying = true;
+  isTransitioning = false;
 };
 
 myVideo.onpause = function () {
   isPlaying = false;
+  isTransitioning = false;
 };
 
 myVideo.onerror = function (event) {
   console.error('Erro de vídeo:', event);
+  isTransitioning = false;
 };
